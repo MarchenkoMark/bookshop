71b0332e2b962b5426439da3cc5e36a9899b1b83
Mark <markwalikdj@gmail.com>
2020-08-07 17:59:43 +0300

    Banner class added and implemented

---
+++ src/app/banner/banner-item.ts
@@ -0,0 +1,7 @@
+export class BannerItem {
+  constructor(
+    public title: string,
+    public text: string,
+    public imageUrl: string,
+    public link: string ) { }
+}
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {BannerItem} from './banner-item';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css', '../app.component.css']
 })
-export class BannerComponent { }
+export class BannerComponent {
+
+  banners = [
+    new BannerItem("Newest rocket science lectures", "Legendary Neil Tyson's heuristics lectures", "./assets/img/banner.png", "#"),
+    new BannerItem("Some other lecture", "One more legendary physics tutorial via same link omg", "./assets/img/banner.png", "#"),
+  ]
+}
