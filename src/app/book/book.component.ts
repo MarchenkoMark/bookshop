27bb35de4e10670f33866f9f6170cb3b0a2858b4
Mark <markwalikdj@gmail.com>
2020-08-16 11:46:02 +0300

    prev and next links

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BookService} from '../book.service';
 import {IBook} from '../book';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-book',
@@ -12,13 +12,24 @@ export class BookComponent implements OnInit {
   public items: IBook[];
   public id: number;
 
-  constructor(private _bookService: BookService, private route: ActivatedRoute) {
+  constructor(private _bookService: BookService, private route: ActivatedRoute, private router: Router) {
     this._bookService.getBooks()
       .subscribe(data => this.items = data);
   }
 
   ngOnInit(): void {
-    this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    //this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.route.paramMap.subscribe((params: ParamMap) =>
+      this.id = parseInt(params.get('id'))
+    )
   }
 
+  goNext() {
+    let nextId = this.id + 1;
+    this.router.navigate(['/merch', nextId]);
+  }
+  goPrev() {
+    let prevId = this.id - 1;
+    this.router.navigate(['/merch', prevId]);
+  }
 }
